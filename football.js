dbb06cae0db05c5ee31305bcb252cddb419587fa
Dib <126925086+RahulDurgavajhala@users.noreply.github.com>
2024-01-22 18:54:05 -0700

    Add files via upload

---
+++ football.js
@@ -0,0 +1,42 @@
+var img = ""
+var status = ""
+objects = []
+
+function preload(){
+    img = loadImage("tvac.jpg") 
+}
+function setup(){
+    canvas = createCanvas(640 , 420)
+    canvas.center()
+    objectDetector = ml5.objectDetector("cocossd" , modalLoaded)
+    document.getElementById("status").innerHTML = "status : detecting objects"
+    
+}
+function draw(){
+    image(img, 0 ,0 ,640 , 420 )
+    if(status !=""){
+        for (let i = 0; i < objects.length; i++) {
+            document.getElementById("status").innerHTML = "status : object is detected"
+            fill("red")
+            percent = Math.floor(objects[i].confidence*100)
+            name = objects[i].label
+            text(name+percent+"%" , objects[i].x , objects[i].y)
+            noFill()
+            stroke("red")
+            rect(objects[i].x , objects[i].y , objects[i].width , objects[i].height)
+        }
+    }
+}
+function modalLoaded(){
+    console.log("modal is loaded")
+    status = true ; 
+    objectDetector.detect(img , gotResults)
+}
+function gotResults(error , results){
+    if (error) {
+console.log(error)        
+    } else {
+        console.log(results)
+        objects = results
+    }
+}
